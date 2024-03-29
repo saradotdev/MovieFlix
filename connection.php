eb63c98be80cc0ef4242b3744c09e47f1fb6948c
saraazizhere <saraaziz180903@gmail.com>
2024-03-29 22:02:34 +0500

    update connection

--- connection.php
+++ connection.php
@@ -1,9 +1,10 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "movierentalsystem";
+//Get Heroku ClearDB connection information
+$servername = "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$username = "jlfcdi7acvjbml6h";
+$password = "vpas1p2oom3j8agv";
+$dbname = "l5fmjlhwzt8a6ja4";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
