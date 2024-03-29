bf6b839e99ec27fe2f8a52c75a6107cb598a9595
saraazizhere <saraaziz180903@gmail.com>
2024-03-29 22:16:07 +0500

    added new connection

--- connection.php
+++ connection.php
@@ -1,6 +1,5 @@
 <?php
 
-//Get Heroku ClearDB connection information
 $servername = "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
 $username = "jlfcdi7acvjbml6h";
 $password = "vpas1p2oom3j8agv";
--- storedataforcustomer.php
+++ storedataforcustomer.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "movierentalsystem";
+$servername = "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$username = "jlfcdi7acvjbml6h";
+$password = "vpas1p2oom3j8agv";
+$dbname = "l5fmjlhwzt8a6ja4";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
