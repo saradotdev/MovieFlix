a7efb1002a0ec312180bd00e819551f631da0495
saraazizhere <saraaziz180903@gmail.com>
2024-03-29 22:30:52 +0500

    updated conn

--- storedataforcustomer.php
+++ storedataforcustomer.php
@@ -4,7 +4,8 @@ $servername = "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
 $username = "jlfcdi7acvjbml6h";
 $password = "vpas1p2oom3j8agv";
 $dbname = "l5fmjlhwzt8a6ja4";
-
+$active_group = 'default';
+$query_builder = TRUE;
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if($_SERVER["REQUEST_METHOD"] == TRUE) {
